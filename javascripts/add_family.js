e7c84143347b7992d21600fb91d164b49bf5e0f3
Rachel Morgan <rachyllmorgan@gmail.com>
2015-08-18 15:34:42 -0500

    attempt at array

--- javascripts/add_family.js
+++ javascripts/add_family.js
@@ -3,12 +3,15 @@ define(function() {
   return {
 
     addFamily: function(newFam)  {
+    // Attempts to create an array for skills
+      var skillsArray = [];
+      var getSkills = skillsArray.push($("#memberSkills").val());
     // Created var for movie
       var newFamily = {
         "name": $("#firstLastName").val(),
         "age": $("#memberAge").val(),
         "gender": $("input[type=radio]:checked").val(),
-        "skills": $("#memberSkills").val(),
+        "skills": $("#memberSkills").val()
       };
     
       console.log("newFamily", newFamily);
